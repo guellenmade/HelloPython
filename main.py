3c45dbeac76ca51580fc40e4ff0253b322984e43
Mailo <mahu.mtb@hotmail.com>
2024-12-05 21:45:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
 print("it's my third program so relax :)")
 test = randint(0, randint(1, 12312313))
-print(test)
+print("here is your random Number" + test)
 input("press enter to close")
